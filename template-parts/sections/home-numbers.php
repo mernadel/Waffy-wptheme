466ae4171218c13226cb9f85042482d3b3dcd10c
mernadel <mernaadel00098@gmail.com>
2025-05-12 12:00:16 +0300

    12-5

--- template-parts/sections/home-numbers.php
+++ template-parts/sections/home-numbers.php
@@ -47,6 +47,127 @@
 
 
 
+<!-- Intermediary CTA Section -->
+<section class="intermediary-cta-section section-start">
+  <div class="container l-grid l-grid--2-cols-md l-grid--align-center">
+    <div class="l-grid__col intermediary-cta-section__content-col">
+      <h2 class="start-selling__title"> <span> هــــل أنـــت وسيـــــط مــــوثـوق    </span> 
+      <br>
+      فـــــــــــي أي مــــــــــــــــجــــــــــــــــــــال؟
+
+      </h2>
+      <p class="home-hero__subtitle mb-15">
+        انضم إلينا الآن، وضاعف أرباحك، واضمن عمولتك، مع ضمان لأموال الطرفين في المعاملات التي تديرها بأمان وسهولة.
+      </p>
+      <div class="home-hero__cta-container text-center">
+        <a href="#" class="home-hero__cta-button d-block">سجل الآن</a>
+      </div>
+    </div>
+    <div class="l-grid__col intermediary-cta-section__image-col">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/intermediary.png" alt="شخص يحمل جهاز لوحي يعرض منصة وفي" class="intermediary-cta-section__image">
+    </div>
+   
+  </div>
+</section>
+<!-- Intermediary CTA Section End -->
+<!-- faq section start -->
+<section class="faq-section section-start ">
+    <div class="container">
+      <div class="home-hero__step-title">
+        <h1 class="main-hero__title1">
+       الأسئلة الشائعة
+        </h1>
+      </div> <!-- Closing div for home-hero__step-title -->
+      <div class="accordion">
+        <div class="accordion-item">
+          <button id="accordion-button-1" aria-expanded="false">
+            <span class="accordion-title">ما هو وفّي وما الخدمة التي يقدمها؟</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              تقدم وفي خدمة الضمان وحماية الأموال (كطرف مستقل) لكل من طرفي المعاملة (البائع والمشتري) لحين إنهاء إجراءات المعاملة وبالشروط المتفق عليها بين الأطراف
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-2" aria-expanded="false">
+            <span class="accordion-title">كيف يمكنني التسجيل في وفّي؟</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              يمكنك التسجيل عبر الموقع أو التطبيق للاستفادة من الخدمة واتباع الخطوات الموضحة لإنشاء حساب جديد، كما يمكنك التواصل مع خدمة العملاء لمزيد من المساعدة أو في حال الرغبة بالتسجيل بميزة الوسيط الضامن (وفي أعمال) المخصص للوسطاء بكافة المجالات
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-3" aria-expanded="false">
+            <span class="accordion-title">هل وفّي آمن؟
+
+            </span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              وفي عبارة عن خدمة معتمدة من شركة سعودية مسجلة لدى وزارة التجارة باسم (شركة وفي للتقنية المالية) ورقم سجل “403043377” وتقدم الخدمة من خلال بنوك سعودية ( الراجحي – الإنماء – بنك الخليج الدولي ) وتستخدم بوابات دفع أمنة ( ميسر – هايبر باي ) ومعتمدة من البنك المركزي لضمان الأموال وإرسالها
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-4" aria-expanded="false">
+            <span class="accordion-title">ما هي طرق الدفع المتاحة عبر وفّي؟</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              يمكن استخدام طرق دفع متعددة مثل بطاقات الائتمان (فيزا – مدى – مساتر كارد) وابل باي والتحويل البنكي وخدمة التقسيط من خلال Tabby
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-5" aria-expanded="false">
+            <span class="accordion-title">ماهي رسوم خدمة وفّي ومن يقوم بدفعها وهل الرسوم شاملة عمولة حراج؟
+
+            </span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              عمولتنا ٤٪ من قيمة المبلغ المدفوع لماذا يتم أخذ عمولة؟ لأن خدمة وفي تقدم للطرفين للحفاظ على جودة وقيمة الخدمة المقدمة بالحفاظ على القيمة المالية المدفوعة مقابل رسوم رمزية تخصم من قيمة المعاملة نفسها. كما أن رسوم خدمة وفي لا تشمل أي معاملات أو رسوم خارج المنصة
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- faq section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   <!-- ncreament counter script -->
   <script>
 
@@ -83,3 +204,26 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 </script>
+<!-- faq script -->
+<script>
+
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+
+
+
+  </script>
