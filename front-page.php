df0eb2dcc5684b213e7fd61c301833064c941d9e
mernadel <mernaadel00098@gmail.com>
2025-05-24 23:22:55 +0300

    feat: add seller/buyer tab section with modal and responsive styles

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@ require get_template_directory() . '/template-parts/sections/home-seller_buyer_t
 require get_template_directory() . '/template-parts/sections/home-cta_form.php';
 require get_template_directory() . '/template-parts/sections/home-featured_posts.php';
 require get_template_directory() . '/template-parts/sections/home-numbers.php';
-require get_template_directory() . '/template-parts/sections/home-cta_signup.php';
+// require get_template_directory() . '/template-parts/sections/home-cta_signup.php';
 require get_template_directory() . '/template-parts/sections/newsletter.php';
 ?>
 <?php get_footer(); ?>
